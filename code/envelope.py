bec026d96fe5206343cb0d0a733854f953f9f3cc
ACGuerrero <acastillo@ciencias.unam.mx>
2024-02-13 15:10:29 +0100

    tex et code

---
+++ code/envelope.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def hl_envelopes_idx(s, dmin=1, dmax=1, split=False):
+    """
+    Input :
+    s: 1d-array, data signal from which to extract high and low envelopes
+    dmin, dmax: int, optional, size of chunks, use this if the size of the input signal is too big
+    split: bool, optional, if True, split the signal in half along its mean, might help to generate the envelope in some cases
+    Output :
+    lmin,lmax : high/low envelope idx of input signal s
+    """
+
+    # locals min      
+    lmin = (np.diff(np.sign(np.diff(s))) > 0).nonzero()[0] + 1 
+    # locals max
+    lmax = (np.diff(np.sign(np.diff(s))) < 0).nonzero()[0] + 1 
+    
+    if split:
+        # s_mid is zero if s centered around x-axis or more generally mean of signal
+        s_mid = np.mean(s) 
+        # pre-sorting of locals min based on relative position with respect to s_mid 
+        lmin = lmin[s[lmin]<s_mid]
+        # pre-sorting of local max based on relative position with respect to s_mid 
+        lmax = lmax[s[lmax]>s_mid]
+
+    # global min of dmin-chunks of locals min 
+    lmin = lmin[[i+np.argmin(s[lmin[i:i+dmin]]) for i in range(0,len(lmin),dmin)]]
+    # global max of dmax-chunks of locals max 
+    lmax = lmax[[i+np.argmax(s[lmax[i:i+dmax]]) for i in range(0,len(lmax),dmax)]]
+    
+    return lmin,lmax
+
+
+if __name__ == "__main__":
+    data_Nio = np.loadtxt('data/Scanning_900GHZ-1200GHZ_NIO.txt')
+    x = data_Nio[:,3]
+    y = data_Nio[:,2]
+
+    # Now we get the high and low envelopes
+    lmin, lmax = hl_envelopes_idx(y, dmin=5, dmax=4, split=False)
+    # Plotting
+    plt.plot(x, y, label='Original Data')
+    plt.plot(x[lmax], y[lmax], label='High Envelope')
+    plt.legend()
+    plt.savefig('figures/testing.png')
+    plt.close()
