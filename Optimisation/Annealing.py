3aa333d8a7837c6f4c9d06ad95e877dbddb5f90f
unknown <victor.leroy92@gmail.com>
2024-02-24 11:53:31 +0100

    Test concluant

--- Optimisation/Annealing.py
+++ Optimisation/Annealing.py
@@ -4,7 +4,9 @@ import scipy as sc
 
 D=np.zeros((6,6))
 #PARIS 1, NANCY 2, STRASBOURG 3, MULHOUSE 4, BESANCON 5, DIJON 6
-D[0,1]=312;D[0,2]=445;D[0,3]=440;D[0,4]=365;D[0,5]=290
+
+#D[0,1]=312;D[0,2]=445;D[0,3]=440;D[0,4]=365;D[0,5]=290
+D[0,1]=815;D[0,2]=757;D[0,3]=850;D[0,4]=992;D[0,5]=1105
 D[1,2]=133;D[1,3]=161;D[1,4]=184;D[1,5]=191
 D[2,3]=102;D[2,4]=227;D[2,5]=282
 D[3,4]=130;D[3,5]=204
@@ -65,6 +67,6 @@ for i in range(5): #boucle for des permutations principales (le zero du début v
                         iii=ii;jjj=jj
 
 print("Le chemin optimal prend",compare,"kms", " calculé en :",Np+Np2,"nombre d'essais"
-      "  Il correspond à la permuation",iii,jjj) #ça marche !!
+      "  Il correspond à la permuation",iii,jjj) 
 
 
