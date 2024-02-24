608ee2287f3e6e84012e13a2f6fca4824490cbf6
unknown <victor.leroy92@gmail.com>
2024-02-24 12:15:27 +0100

    j'ai fais un graph des villes

--- Optimisation/Annealing.py
+++ Optimisation/Annealing.py
@@ -6,7 +6,7 @@ D=np.zeros((6,6))
 #PARIS 1, NANCY 2, STRASBOURG 3, MULHOUSE 4, BESANCON 5, DIJON 6
 
 #D[0,1]=312;D[0,2]=445;D[0,3]=440;D[0,4]=365;D[0,5]=290
-D[0,1]=815;D[0,2]=757;D[0,3]=850;D[0,4]=992;D[0,5]=1105
+D[0,1]=815;D[0,2]=757;D[0,3]=850;D[0,4]=992;D[0,5]=1105 #Test remplacement Paris par Berlin 
 D[1,2]=133;D[1,3]=161;D[1,4]=184;D[1,5]=191
 D[2,3]=102;D[2,4]=227;D[2,5]=282
 D[3,4]=130;D[3,5]=204
@@ -69,4 +69,11 @@ for i in range(5): #boucle for des permutations principales (le zero du début v
 print("Le chemin optimal prend",compare,"kms", " calculé en :",Np+Np2,"nombre d'essais"
       "  Il correspond à la permuation",iii,jjj) 
 
+paris=[1,6.5];dijon=[6.5,2];besançon=[8.7,1.5];mulhouse=[11.5,3.1];strasbourg=[12.3,5.5];nancy=[9,6.9]
 
+ville=np.zeros((6,2))
+ville[0,:]=paris;ville[1,:]=dijon;ville[2,:]=besançon;ville[3,:]=mulhouse;ville[4,:]=strasbourg;ville[5,:]=nancy
+plt.figure()
+plt.plot(ville[:,0],ville[:,1])
+plt.scatter(ville[:,0],ville[:,1])
+plt.show()
