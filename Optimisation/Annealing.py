59009fc4a13104fcd8d32f12092027ea854f22f1
unknown <victor.leroy92@gmail.com>
2024-02-24 11:42:54 +0100

    Pour le set de 6 villes l'algorithme semble marcher

--- Optimisation/Annealing.py
+++ Optimisation/Annealing.py
@@ -14,12 +14,18 @@ T=np.arange(1,7,dtype=int)-1
 TT=np.zeros((2,6),dtype=int) #Tableau valeur ordre 
 TT[0,:]=T
 TT[1,:]=T
+print(TT)
 
+def facto(n):
+    if n == 0 :
+        return 1 
+    else:
+        return n*facto(n-1)
 
 def permutation(TT,j,k):
     a=0;b=0
     a=TT[0,j];b=TT[0,k];TT[0,j]=b;TT[0,k]=a
-    return TT
+    return TT,j,k
 
 def KM(TT):
     DT=np.zeros(6,dtype=int)
@@ -27,14 +33,38 @@ def KM(TT):
     for i in range(5):
         DT[i]=DD[TT[0,i],TT[0,i+1]]
         KM=DT[i]+KM
-    return KM
+    return KM,TT
 
-compare=np.zeros((6,6))
+compare=KM(TT)[0]
+#print(compare)
+Np=0 #nombre de permuations principales 
+Np2=0 #nombre de sous-permutations 
+iii=jjj=0
+TT1=np.zeros((2,6),dtype=int)
+for i in range(5): #boucle for des permutations principales (le zero du début va jusque la position de la dernière ville)
+    TT1,ii,jj=permutation(TT,i,i+1) #permutation principale
+    #print(TT1,i,KM(TT1)[0])
+    Np=Np+1 
+    if KM(TT1)[0]<=compare:
+        compare=KM(TT1)[0] #On garde en mémoire la permutation seulement si elle est pertinante (plus courte)
+    if i >= 1 : #à partir de la position 2 du zeros on démarre les sous-permutations
+        while Np2<=facto(i+2): #Tant que le nombre de sous permutations n'est pas égal au nombre de sous-permut on continu de tester
+            j=np.arange(i+1) # Indices des sous-permutations possibles. 
+            for p in range(i):
+                TT1,ii,jj=permutation(TT1,j[p],j[p+1])
+                #print(TT1,KM(TT1)[0])
+                Np2=Np2+1
+                if KM(TT1)[0]<=compare:
+                    compare=KM(TT1)[0]
+                    iii=ii;jjj=jj
+                if p+2 < i :
+                    TT1,ii,jj=permutation(TT1,j[p],j[p+2])
+                    Np2=Np2+1
+                    if KM(TT1)[0]<=compare:
+                        compare=KM(TT1)[0]
+                        iii=ii;jjj=jj
 
-for i in range(2,6):
-    compare[0,0]=KM(TT)    
-    TT=permutation(TT,i-1,i)
-    print(TT)
-    compare[i,0]=KM(permutation(TT,i-1,i))
+print("Le chemin optimal prend",compare,"kms", " calculé en :",Np+Np2,"nombre d'essais"
+      "  Il correspond à la permuation",iii,jjj) #ça marche !!
 
-print(compare)
+
